e7637e3c16084a749ee664b392ccb47bc4159bc3
DESKTOP-KA9I336\교육생42 <affogato128@gmail.com>
2023-11-09 19:33:41 +0900

    [fix] GatwayApplication

--- src/main/java/shparos/gateway/GatewayApplication.java
+++ src/main/java/shparos/gateway/GatewayApplication.java
@@ -12,6 +12,42 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(GatewayApplication.class, args);
+//	}
+//
+//	@Bean
+//	public CorsConfigurationSource corsConfigurationSource() {
+//		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		CorsConfiguration corsConfiguration = new CorsConfiguration();
+//		//↓ access를 허용할 Origin을 설정함 (모든 Origin 허용)
+//		//  정규 표현식 패턴을 사용하여 여러 오리진을 한번에 허용할 수 있다.
+//		corsConfiguration.addAllowedOriginPattern("*");
+//		//↓ Authorization, Content-Type, X-Requested-With등의 request헤더를 설정한다.(브라우저에서 수행한다.)
+//		corsConfiguration.addAllowedHeader("*");
+//		//↓ 클라이언트에서 접근가능하게 할 헤더를 명시적으로 지정한다.
+//		// + 클라이언트에서 접근할수 있고 노출시킬 헤더를 설정
+//		corsConfiguration.addExposedHeader("*");
+//		//↓ addAllowedOriginPattern과는 다르게 접근을 허용할 특정 origin을 명시적으로 지정한다.
+//		//↓ localhost:3000에서 오는 요청을 허용한다.
+//		corsConfiguration.addAllowedOrigin("http://localhost:3000");
+//		corsConfiguration.addAllowedOrigin("http://localhost:8000");
+//		//↓GET,POST...등등 허용할 HTTP method를 설정한다.
+//		corsConfiguration.addAllowedMethod("*");
+//		//↓자격증명 설정
+//		corsConfiguration.setAllowCredentials(true);
+//		//↓모든 경로(/**)에 대해 CORS구성을 적용한다.
+//		//위에서 설정한 corsConfiguration을 주입한다.
+//		source.registerCorsConfiguration("/**", corsConfiguration);
+//
+//		return source;
+//	}
+//
+//	@Bean
+//	public CorsWebFilter corsWebFilter() {
+//		return new CorsWebFilter(corsConfigurationSource());
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
@@ -21,23 +57,12 @@ public class GatewayApplication {
 	public CorsConfigurationSource corsConfigurationSource() {
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		//↓ access를 허용할 Origin을 설정함 (모든 Origin 허용)
-		//  정규 표현식 패턴을 사용하여 여러 오리진을 한번에 허용할 수 있다.
 		corsConfiguration.addAllowedOriginPattern("*");
-		//↓ Authorization, Content-Type, X-Requested-With등의 request헤더를 설정한다.(브라우저에서 수행한다.)
 		corsConfiguration.addAllowedHeader("*");
-		//↓ 클라이언트에서 접근가능하게 할 헤더를 명시적으로 지정한다.
-		// + 클라이언트에서 접근할수 있고 노출시킬 헤더를 설정
 		corsConfiguration.addExposedHeader("*");
-		//↓ addAllowedOriginPattern과는 다르게 접근을 허용할 특정 origin을 명시적으로 지정한다.
-		//↓ localhost:3000에서 오는 요청을 허용한다.
 		corsConfiguration.addAllowedOrigin("http://localhost:3000");
-		//↓GET,POST...등등 허용할 HTTP method를 설정한다.
 		corsConfiguration.addAllowedMethod("*");
-		//↓자격증명 설정
 		corsConfiguration.setAllowCredentials(true);
-		//↓모든 경로(/**)에 대해 CORS구성을 적용한다.
-		//위에서 설정한 corsConfiguration을 주입한다.
 		source.registerCorsConfiguration("/**", corsConfiguration);
 
 		return source;
@@ -47,5 +72,6 @@ public class GatewayApplication {
 	public CorsWebFilter corsWebFilter() {
 		return new CorsWebFilter(corsConfigurationSource());
 	}
+
 
 }
